f4a03517d4b9d2acb3fedec8ed444770065dd2c5
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2021-09-18 17:57:37 -0400

    mean bias was o-f, now f-o

--- spread_skill.py
+++ spread_skill.py
@@ -112,7 +112,7 @@ class stats:
         return self.ds.max(dim=dim) - self.ds.min(dim=dim)
 
     def mean_bias(self):
-        return np.mean(self.obs[self.obs_var]-self.ds[self.ds_var])
+        return np.mean(self.ds[self.ds_var]-self.obs[self.obs_var])
     
     def valid_sample_space(self, dim='number', save=True):
         if os.path.exists(f"{self.analysis_dir}/stats/vss_{self.ds_var}_{str(self.ds['time'].values.astype('datetime64[D]'))}"):
