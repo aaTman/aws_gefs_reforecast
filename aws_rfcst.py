b1cdf4c6b1c2ad067530ed9801bb7b437afb539c
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2021-09-18 22:17:26 -0400

    ahhh

--- aws_rfcst.py
+++ aws_rfcst.py
@@ -90,7 +90,7 @@ def load_xr_with_datatype(fpath, output_file, datatype, int_step=1, hour_step=6)
         backend_kwargs={
             'filter_by_keys':{'dataType':datatype},
             },
-            chunks={'step':10}).isel(step=slice(int_step,None,2))
+            chunks={'step':10})
     ds.attrs = {}
     
     if datatype == 'cf':
@@ -105,7 +105,7 @@ def load_xr_with_datatype(fpath, output_file, datatype, int_step=1, hour_step=6)
             backend_kwargs={
                 'filter_by_keys':{'dataType':datatype},
                 },
-                chunks={'step':10}).isel(step=slice(int_step,None,2)) 
+                chunks={'step':10}) 
         if datatype == 'cf':
             try:
                 ds = ds.sel(number=0)
